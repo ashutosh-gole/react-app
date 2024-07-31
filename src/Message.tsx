beac809ab8416faddfa65a20ca5fc24638db5c33
ashutosh-gole <ashutoshsgole@gmail.com>
2024-07-31 18:27:18 +0530

    Initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+//PascalCasing used
+
+function Message() {
+  //JSX: Javascript XML
+  const name = "Ashutosh";
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
+}
+
+export default Message;
