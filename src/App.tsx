c07f9febffc41b238cb7eaa9f0a466dec16f3f44
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-01 19:13:44 +0530

    passing children - like content projection in angular

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,12 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let states = [
-    "Maharashtra",
-    "Goa",
-    "Karnataka",
-    "Uttar Pradesh",
-    "Gujrat",
-    "Madhy Pradesh",
-    "Rajsthan",
-    "Odisa",
-    "Uttarakhand",
-  ];
-  let heading = "List Group - States - Passing Functions Via Props";
-
-  const handleSelectItem = (index: number, state: string) => {
-    console.log("CHILD TO PARENT index, state===== ", index, state);
-  };
-
   return (
     <>
-      <h1>CHILD TO PARENT METHOD 1</h1>
-      <ListGroup
-        states={states}
-        heading={heading}
-        onSelectItem={(index, state) => handleSelectItem(index, state)}
-      />
-
-      <h1>CHILD TO PARENT METHOD 2</h1>
-      <ListGroup
-        states={states}
-        heading={heading}
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        <b>Method 1</b>
+      </Alert>
+      <Alert children={<b>Method 2</b>}></Alert>
     </>
   );
 }
