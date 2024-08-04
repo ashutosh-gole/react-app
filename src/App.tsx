1a98b1e71b6eef7b27e6848fd6c3ab2da5242c8c
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-04 21:27:15 +0530

    sharing states with components in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
+import { useState } from "react";
 import "./App.css";
-import Like from "./components/Like";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 function App() {
-  const handlerIconClick = (status: boolean) => {
-    console.log("CHILD TO PARENT handlerIconClick status ===== ", status);
-  };
+  const [cartItems, setCartItems] = useState(["Product 1", "Product 2"]);
 
   return (
     <>
-      <Like onIconClick={handlerIconClick} />
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
     </>
   );
 }
