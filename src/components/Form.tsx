555173a9ffe1031388e3ec99044ef68111b17c26
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-05 19:20:10 +0530

    controlled components in forms using useState hook

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,11 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 
 const Form = () => {
+  const [person, setPerson] = useState({ name: "Ashutosh G", age: 28 });
+
   const handelSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Submit Clicked");
+    console.log("person === ", person);
   };
 
   return (
@@ -15,14 +17,30 @@ const Form = () => {
           <label htmlFor="name" className="form-label">
             Name
           </label>
-          <input type="text" className="form-control" id="name" />
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={person.name}
+            onChange={(event) =>
+              setPerson({ ...person, name: event.target.value })
+            }
+          />
         </div>
 
         <div className="mb-3">
           <label htmlFor="age" className="form-label">
             Age
           </label>
-          <input type="number" className="form-control" id="age" />
+          <input
+            type="number"
+            className="form-control"
+            id="age"
+            value={person.age}
+            onChange={(event) =>
+              setPerson({ ...person, age: parseInt(event.target.value) })
+            }
+          />
         </div>
 
         <button type="submit" className="btn btn-primary">
