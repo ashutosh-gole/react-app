7da52ea95f37ce8d9e1b66c593f42b4dd385f90f
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-06 13:27:21 +0530

    schema based validation using zod library

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,70 +1,76 @@
-import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormData {
-  name: string;
-  age: number;
-}
+// schema using zod package
+const UserSchema = z.object({
+  name: z
+    .string({ required_error: "Name is required" })
+    .min(5, { message: "Must be 5 or more characters long" }),
+  age: z
+    .number({
+      required_error: "Age is required",
+      invalid_type_error: "Age must be a number",
+    })
+    .nonnegative({ message: "Age cannot be negative" }), // nonnegative method used
+});
+
+// extract the inferred type
+type FormData = z.infer<typeof UserSchema>;
 
 const Form = () => {
-  // nested destructuring in js/ts
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({
+    resolver: zodResolver(UserSchema),
+    defaultValues: {
+      name: "Rahul",
+      age: 30,
+    },
+  });
 
-  // your form logic
-  const submitClick = (data: FieldValues) => {
+  const submitClick = (data: FormData) => {
     console.log("data === ", data);
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit((data) => submitClick(data))}>
-        <h1>
-          React Form: manage form with <i>react-hook-form</i> library
-        </h1>
+    <form onSubmit={handleSubmit(submitClick)}>
+      <h1>React Form: schema based validation with zod</h1>
 
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            {...register("name", { required: true, minLength: 3 })}
-          />
-          {errors.name?.type === "required" && (
-            <p className="pt-2 text-danger">The name field is required</p>
-          )}
-          {errors.name?.type === "minLength" && (
-            <p className="pt-2 text-danger">
-              The name must be at least 3 characters
-            </p>
-          )}
-        </div>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          {...register("name")}
+        />
+        {errors.name && (
+          <p className="pt-2 text-danger">{errors.name.message}</p>
+        )}
+      </div>
 
-        <div className="mb-3">
-          <label htmlFor="age" className="form-label">
-            Age
-          </label>
-          <input
-            type="number"
-            className="form-control"
-            id="age"
-            {...register("age", { required: true, min: -1 })}
-          />
-          {errors.age?.type === "required" && (
-            <p className="pt-2 text-danger">The age field is required</p>
-          )}
-        </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          type="number"
+          className="form-control"
+          id="age"
+          {...register("age", { valueAsNumber: true })}
+        />
+        {errors.age && <p className="pt-2 text-danger">{errors.age.message}</p>}
+      </div>
 
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
   );
 };
 
