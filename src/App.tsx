3af10d94193cb1ce3e47cceec7768f7a09691413
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-07 16:21:39 +0530

    useEffect hook added in code

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,20 @@
-import { useState } from "react";
-import "./App.css";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import { useEffect, useRef } from "react";
 
 function App() {
-  // set the default selected category as "Utilities" for practice
-  const [selectedCategory, setSelectedCategory] = useState("Utilities");
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 20, category: "Utilities" },
-    { id: 3, description: "ccc", amount: 30, category: "Utilities" },
-    { id: 4, description: "ddd", amount: 40, category: "Groceries" },
-    { id: 5, description: "eee", amount: 50, category: "Utilities" },
-  ]);
-  const visibleExpenses =
-    selectedCategory && selectedCategory !== "All Categories"
-      ? expenses.filter((expense) => expense.category === selectedCategory)
-      : expenses;
+  const ref = useRef<HTMLInputElement>(null);
+
+  // after renderer => we use useEffect(() => {}) to make component pure
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  });
+
+  useEffect(() => {
+    document.title = "React App";
+  });
 
   return (
     <>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmitClick={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        />
-      </div>
-
-      <div className="mb-5">
-        <ExpenseFilter
-          defaultSelectedCategory={selectedCategory}
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) =>
-          setExpenses(expenses.filter((expense) => expense.id !== id))
-        }
-      />
+      <input ref={ref} type="text" className="form-control"></input>
     </>
   );
 }
