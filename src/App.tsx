33dacfc5f16b77ac8c52484054f8cf2babe7236e
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-08 12:27:19 +0530

    error handling done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -8,20 +8,27 @@ interface User {
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .get<User[]>("https://jsonplaceholder.typicode.com/users2")
       .then((res) => {
         console.log("1 res === ", res);
         console.log("2 users === ", users);
         setUsers(res.data);
+      })
+      .catch((err: AxiosError) => {
+        console.log("err ==== ", err);
+        setError(err.message);
       });
   }, []);
 
   return (
     <>
       <h1>Fetch data using axios library</h1>
+
+      {error && <p className="text-danger">{error}</p>}
 
       <ul>
         {users.map((user) => (
