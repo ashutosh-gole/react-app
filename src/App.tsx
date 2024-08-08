4a548b19da8224785e0d4b54ae717735fb2dc0fb
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-08 14:42:13 +0530

    loading indicator added when fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -9,24 +9,28 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setIsLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
       .then((res) => {
-        console.log("1 res === ", res);
-        console.log("2 users === ", users);
         setUsers(res.data);
+        setIsLoading(false);
       })
       .catch((err: AxiosError) => {
-        console.log("err ==== ", err);
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setIsLoading(false);
       });
+    // .finally(() => setIsLoading(false));
+    // in strict mode finally() block not executed so we added 2 times setIsLoading(false); in our code
+    // best practice is to add setIsLoading(false); code in finally block
 
     return () => controller.abort();
   }, []);
@@ -36,6 +40,12 @@ function App() {
       <h1>Fetch data using axios library</h1>
 
       {error && <p className="text-danger">{error}</p>}
+
+      {isLoading && (
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      )}
 
       <ul>
         {users.map((user) => (
