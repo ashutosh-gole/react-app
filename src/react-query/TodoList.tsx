c8d1fd48d55e3ea13d0646349aa1c1ec6b0b1c78
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-15 23:00:26 +0530

    creating custom query hook

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,52 +1,26 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-  userId: number;
-}
+import useTodos from "../hooks/useTodos";
 
 const TodoList = () => {
-  const fetchTodos = () =>
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
+  const { data: todos, error, isLoading } = useTodos();
 
-  const {
-    data: todos,
-    error,
-    isLoading,
-  } = useQuery<Todo[], Error>({
-    queryKey: ["todos"],
-    queryFn: fetchTodos,
-  });
+  if (isLoading)
+    return (
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    );
 
-  // if (error) return <p className="text-danger">{error.message}</p>;
-
-  // if (isLoading)
-  //   return (
-  //     <div className="spinner-border" role="status">
-  //       <span className="visually-hidden">Loading...</span>
-  //     </div>
-  //   );
+  if (error) return <p className="text-danger">{error.message}</p>;
 
   return (
     <>
       <h1>Todos:</h1>
 
-      {error && <p className="text-danger">{error.message}</p>}
-
-      {isLoading && (
-        <div className="spinner-border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-      )}
-
-      <ul>
+      <ul className="list-group">
         {todos?.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
         ))}
       </ul>
     </>
