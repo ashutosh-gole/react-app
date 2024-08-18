7473a08846a7a074f9c599b012f7b3224920cadf
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-18 18:47:53 +0530

    state logic with reducers

--- src/state-management/Counter.tsx
+++ src/state-management/Counter.tsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useReducer } from "react";
+import counterReducers from "./reducers/counterReducers";
 
 const Counter = () => {
-  const [value, setValue] = useState(0);
+  const [value, dispatch] = useReducer(counterReducers, 0);
 
   return (
     <div>
       Counter ({value})
       <button
         className="btn btn-primary mx-1"
-        onClick={() => setValue(value + 1)}
+        onClick={() => dispatch({ type: "INCREMENT" })}
       >
         Increment
       </button>
-      <button className="btn btn-primary mx-1" onClick={() => setValue(0)}>
+      <button
+        className="btn btn-primary mx-1"
+        onClick={() => dispatch({ type: "RESET" })}
+      >
         Reset
       </button>
     </div>
