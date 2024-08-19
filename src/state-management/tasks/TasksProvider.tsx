7560ba98e11e7d3d34c5e826839fd916ba3be8d8
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-19 23:44:38 +0530

    organizing code for scalability and maintainability

--- src/state-management/tasks/TasksProvider.tsx
+++ src/state-management/tasks/TasksProvider.tsx
@@ -1,6 +1,35 @@
 import { ReactNode, useReducer } from "react";
 import TasksContext from "./tasksContext";
-import tasksReducer from "./tasksReducer";
+
+export interface Task {
+  id: number;
+  title: string;
+}
+
+interface AddTask {
+  type: "ADD";
+  task: Task;
+}
+
+interface DeleteTask {
+  type: "DELETE";
+  taskId: number;
+}
+
+export type TaskAction = AddTask | DeleteTask;
+
+const tasksReducer = (tasks: Task[], action: TaskAction): Task[] => {
+  switch (action.type) {
+    case "ADD":
+      return [action.task, ...tasks];
+
+    case "DELETE":
+      return tasks.filter((task) => task.id !== action.taskId);
+
+    default:
+      return tasks;
+  }
+};
 
 interface Props {
   children: ReactNode;
