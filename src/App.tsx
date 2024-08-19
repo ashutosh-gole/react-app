902c11f5b94e2c662962d1b86b48afaf62fc9e95
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-19 19:54:08 +0530

    creating custom provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import { useReducer } from "react";
+import AuthProvider from "./state-management/AuthProvider";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
-import AuthContext from "./state-management/context/authContext";
 import TasksContext from "./state-management/context/tasksContext";
-import authReducer from "./state-management/reducers/authReducer";
 import tasksReducer from "./state-management/reducers/tasksReducer";
 
 function App() {
   const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-  const [user, authDispatch] = useReducer(authReducer, "");
 
   return (
-    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+    <AuthProvider>
       <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
         <NavBar />
         <HomePage />
       </TasksContext.Provider>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 }
 
