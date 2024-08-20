598de9e7098771eb01fb59c5bafca39c7d2f21ad
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-20 17:24:30 +0530

    managing state with zustand

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,12 +1,15 @@
 import { LoginStatus } from "./auth";
+import useCounterStore from "./counter/store";
 import useTasks from "./tasks/useTasks";
 
 const NavBar = () => {
   const { tasks } = useTasks();
+  const { counter } = useCounterStore();
 
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">{tasks.length}</span>
+      <span className="badge text-bg-secondary">Counter: {counter}</span>
+      <span className="badge text-bg-secondary">Tasks: {tasks.length}</span>
       <LoginStatus />
     </nav>
   );
