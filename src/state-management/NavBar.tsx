79c8dd0a3b093b2b4f6ec36a537a9ac4210c5114
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-20 18:06:19 +0530

    preventing unnecessary renders with selectors

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -4,7 +4,9 @@ import useTasks from "./tasks/useTasks";
 
 const NavBar = () => {
   const { tasks } = useTasks();
-  const { counter } = useCounterStore();
+  const counter = useCounterStore((s) => s.counter);
+
+  console.log("Navbar render == ", counter);
 
   return (
     <nav className="navbar d-flex justify-content-between">
