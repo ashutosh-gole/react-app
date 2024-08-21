36cab375f72729e42ebdb3d0841a78bfd688c6e8
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-21 15:58:09 +0530

    private routes - redirect using <Navigate /> component

---
+++ src/routing/hooks/useAuth.ts
@@ -0,0 +1,6 @@
+const useAuth = () => {
+    // return ({ user: { id: 1, name: "Ashutosh G" } });
+    return ({ user: null });
+}
+
+export default useAuth; 
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -5,6 +5,7 @@ import Layout from "./Layout";
 import UserDetail from "./UserDetail";
 import UserPage from "./UserPage";
 import ErrorPage from "./ErrorPage";
+import LoginPage from "./LoginPage";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { path: "", element: <HomePage /> }, // or { index: true, element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
       {
         path: "users",
         element: <UserPage />,
