d05bb18961ddc364e3c54430a57fc57b7e927229
ashutosh-gole <ashutoshsgole@gmail.com>
2024-08-21 15:18:34 +0530

    styling the active link

--- src/routing/NavBar.tsx
+++ src/routing/NavBar.tsx
@@ -13,7 +13,16 @@ const NavBar = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <NavLink to="/" className="nav-link">
+              {/* <NavLink to="/" className="nav-link">
+                Home
+              </NavLink> */}
+              {/* manual active class adding */}
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "active nav-link" : "nav-link"
+                }
+              >
                 Home
               </NavLink>
             </li>
